00d910bcd26fcdc372e46b8baf3fdeb75b394f28
Chroniclse <wassupdanger33@gmail.com>
2025-07-16 14:29:44 -0700

    Reset Backend

--- backend/app/utils/elo.py
+++
@@ -1,9 +0,0 @@
-# Elo rating calculation functions
-
-def calculate_elo(winner_rating, loser_rating, k=32):
-    """Calculate new Elo ratings for winner and loser."""
-    expected_win = 1 / (1 + 10 ** ((loser_rating - winner_rating) / 400))
-    winner_new = winner_rating + k * (1 - expected_win)
-    loser_new = loser_rating + k * (0 - (1 - expected_win))
-    return round(winner_new), round(loser_new)
-
